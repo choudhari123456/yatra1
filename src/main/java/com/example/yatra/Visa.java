9069f1572e0083de5cfbbd358b54951286f45387
anandamohanpm <anandamohanpm@gmail.com>
2022-11-15 22:55:11 +0530

    CodeRefactor

--- src/main/java/com/example/yatra/Visa.java
+++ src/main/java/com/example/yatra/Visa.java
@@ -8,7 +8,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class Visa {
     @GetMapping("/myvisa")
     public String getData(){
-        return "Please submit your visa application here";
+        return "Please submit your visa application here,hhhhhskjdfnkjsdnfk jnsk, loging";
     }
+
 
 }

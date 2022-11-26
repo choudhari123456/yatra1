93f0c3afb0ddacdccc37e04a795f06daba52e950
anandamohanpm <anandamohanpm@gmail.com>
2022-11-26 21:39:27 +0530

    train file

--- src/main/java/com/example/yatra/train.java
+++ src/main/java/com/example/yatra/train.java
@@ -5,10 +5,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class Visa {
-    @GetMapping("/myvisa")
+public class train {
+    @GetMapping("/mytrain")
     public String getData(){
-        return "Please submit your visa application here,welcome";
+        return "Please book your Bus ticket, AALIYA EXPRESS LINES";
     }
 
 }
